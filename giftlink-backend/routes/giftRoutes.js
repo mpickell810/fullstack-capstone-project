d18562b1197e29f1f242172734bbded86ab1f183
mpickell810 <silver066surfer@gmail.com>
2026-08-20 15:27:22 -0400

    adding api endpoints and connecting MongoDB

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,34 +1,35 @@
-router.get('/', async (req, res) => {
+const express = require('express');
+const router express.Router();
+const connectToDatabase = require("../models/db");
+const logger = require('../logger');
+
+// Get all gifts
+router.get('/', async (req, res, next) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
-
+        const db = await connectToDatabase();
         // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
-
+        const collection = db.collection("gifts");
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
-
+        const gifts = await collection.find{{}}.toArray();
         // Task 4: return the gifts using the res.json method
-        res.json(/* {{insert code here}} */);
+        res.json(gifts);
     } catch (e) {
-        console.error('Error fetching gifts:', e);
-        res.status(500).send('Error fetching gifts');
+        logger.console.error('Error fetching gifts:', e)
+        next(e);
     }
 });
 
-router.get('/:id', async (req, res) => {
+// Get a single Gift by ID
+router.get('/:id', async (req, res, next) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
-
+        const db = await connectToDatabase();
         // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
-
+        const collection = db.collection("gifts");
         const id = req.params.id;
-
         // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        // {{insert code here}}
+        const gift = await collection.findOne({ id: id});
 
         if (!gift) {
             return res.status(404).send('Gift not found');
@@ -37,11 +38,9 @@ router.get('/:id', async (req, res) => {
         res.json(gift);
     } catch (e) {
         console.error('Error fetching gift:', e);
-        res.status(500).send('Error fetching gift');
+        next(e);
     }
 });
-
-
 
 // Add a new gift
 router.post('/', async (req, res, next) => {
