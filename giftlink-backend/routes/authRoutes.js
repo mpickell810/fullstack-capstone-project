f834bbf9cb5c88153c810af8ef1484b502fdf63d
mpickell810 <silver066surfer@gmail.com>
2026-08-25 13:41:23 -0400

    adding authRoutes.js

---
+++ giftlink-backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+//Step 1 - Task 2: Import necessary packages
+
+//Step 1 - Task 3: Create a Pino logger instance
+
+dotenv.config();
+
+//Step 1 - Task 4: Create JWT secret
+
+router.post('/register', async (req, res) => {
+//Step 2
+});
+
+module.exports = router;
